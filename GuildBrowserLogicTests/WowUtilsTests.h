99b9a440e5a0b4f395c44c0d3b73b4fa5ad4fbc7
jorperez <perezejea@hotmail.com>
2013-01-21 17:26:09 +0000

    Logic unit test implemented

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Jorge Perez Lahera on 21/01/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
