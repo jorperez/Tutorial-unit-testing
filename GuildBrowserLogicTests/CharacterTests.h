9b81fbfd37f6a7098791b29e6944e12f387d1eca
jorperez <perezejea@hotmail.com>
2013-01-22 12:30:35 +0000

    Character test

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Jorge Perez Lahera on 22/01/2013.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
